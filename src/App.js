996b722bbddf79abae688cfb9f32d627b4cb4cd1
Derek Leafgreen <drl21788@gmail.com>
2022-05-09 07:07:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';    // 1️⃣
+import './App.css';           // 2️⃣
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() {              // 3️⃣
+  const age = 34;
+  const name = "Derek Leafgreen";  // 4️⃣
+  
+  return (                    // 5️⃣
+  <div>
+  <h1 className="title">Hello World!</h1>
+  <p> This is a paragraph of text</p>
+  <p> My name is {name} and I'm {age} years old!!! And I'm "murican!</p>
+  </div>     // 6️⃣
+  );                          // 7️⃣
+}                             // 8️⃣
 
-export default App;
+export default App;           // 9️⃣
